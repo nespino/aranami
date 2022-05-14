71eab263b807e9cfed3857f1eb007a93ad33013b
Your Name <you@example.com>
2022-05-13 21:16:32 -0300

    Add pages

---
+++ pages/soja.php
@@ -0,0 +1,4 @@
+  <h2>Cultivo de Soja</h2>
+  <div>
+    <img src="img/cultivo_soja.jpg" alt="">
+  </div>
---
+++ pages/trigo.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2>Cultivo de Trigo</h2>
+  <div>
+    <img src="img/cultivo_trigo.jpg" alt="">
+  </div>
+</body>
+</html>
